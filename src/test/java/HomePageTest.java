cc8a7612966d2721d485a841a1925a5397c19e05
Aisuluu Zhumabaeva <aisuluujan@gmail.com>
2020-03-24 14:45:48 -0700

    Verify Title

---
+++ src/test/java/HomePageTest.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class HomePageTest {
+
+    public static void main (String[] args)  {
+
+        //Go to the home page  https://kwidos.tk/offer/search
+        //Verify title is equal to Kwidos
+
+        WebDriver driver = new ChromeDriver();
+    }
+}
+
