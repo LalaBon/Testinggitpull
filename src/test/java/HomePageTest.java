dfc9308c9231f92941ac198f1829bb6fe7476a8e
Aisuluu Zhumabaeva <aisuluujan@gmail.com>
2020-03-25 16:34:08 -0700

    Chrome

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -1,19 +1,23 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
 
 public class HomePageTest {
 
-    private static ChromeDriver driver;
+        private static ChromeDriver driver;
 
-    public static void main (String[] args)  {
+        public static void main (String[] args)  {
 
-        //Go to the home page  https://kwidos.tk/offer/search
-        //Verify title is equal to Kwidos
+            //Go to the home page  https://gmail.com
+            //Verify title is equal to Gmail
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("ignore-certificate-errors");
-        driver = new ChromeDriver(options);
-    }
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("ignore-certificate-errors");
+            driver = new ChromeDriver(options);
+        }
+
+
 }
 
